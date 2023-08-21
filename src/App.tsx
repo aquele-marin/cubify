7782acc8ed8a12fa3585e9e145156cafda5f1240
Gabriel Marin <gabrielvbmarin21@gmail.com>
2023-08-20 22:39:11 -0300

    feat: landing page for 3d product show off

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,11 @@
-import { useState, useRef } from "react";
-import { Canvas, ThreeElements, useFrame } from "@react-three/fiber";
-import { OrbitControls, Stars } from "@react-three/drei";
-
-function Box(props: ThreeElements["mesh"]) {
-  const meshRef = useRef<THREE.Mesh>(null!);
-  const [hovered, setHover] = useState(false);
-  const [active, setActive] = useState(false);
-
-  useFrame((_state, delta) => (meshRef.current.rotation.x += delta));
-
-  return (
-    <mesh
-      {...props}
-      ref={meshRef}
-      scale={active ? 1.5 : 1}
-      onClick={() => setActive(!active)}
-      onPointerOver={() => setHover(true)}
-      onPointerOut={() => setHover(false)}
-    >
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
-    </mesh>
-  );
-}
+import { App as Canvas } from "./components/atoms/Canvas";
+import { Overlay } from "./components/atoms/Overlay";
 
 export default function App() {
-  return (
-    <div
-      id="canvas-container"
-      style={{ height: "100vh", backgroundColor: "gray" }}
-    >
-      <Canvas>
-        <OrbitControls />
-        <Stars />
-        <ambientLight />
-        <pointLight position={[10, 10, 10]} />
-        <Box position={[-1.2, 0, 0]} />
-        <Box position={[1.2, 0, 0]} />
-      </Canvas>
-    </div>
-  );
+    return (
+        <>
+            <Canvas />
+            <Overlay />
+        </>
+    );
 }
